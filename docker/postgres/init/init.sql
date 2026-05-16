3266d03035ff451c21fb6e565961e5b0ac521b4e
seungho lee <dooftac712091@icloud.com>
2026-05-16 20:19:21 +0900

    Add updated PostgreSQL configuration, initial database schema, and UserDTO model

--- docker/postgres/init/init.sql
+++ docker/postgres/init/init.sql
@@ -1,5 +1,6 @@
 CREATE TABLE users (
                        id SERIAL PRIMARY KEY,
+                       external_id UUID UNIQUE NOT NULL DEFAULT gen_random_uuid(),
                        username VARCHAR(255) UNIQUE NOT NULL,
                        password VARCHAR(255) NOT NULL,
                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
---
+++ src/main/java/com/seu/seustock/model/UserDTO.java
@@ -0,0 +1,19 @@
+package com.seu.seustock.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+public class UserDTO {
+    private Long id;
+    private UUID externalId;
+    private String username;
+    private String password;
+    private LocalDateTime createdAt;
+}
